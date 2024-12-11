fbd9e79a9aad60dfb4c89c2ce09ae423f53d19f3
Bharath Thiagarajan <Bharath.Thiagarajan@mindoxtechno.com>
2024-12-11 16:38:48 +0530

    added error handling and few minor adjustment

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
 import "fmt"
+import "os"
+import "strconv"
+import "errors"
+
+const accountBalanceFileName = "balanceTxtFile.txt"
+
+func ReadFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFileName)
+	if err != nil {
+		return 1000, errors.New("Failed to read file")
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("Failed to parse file")
+	}
+	return balance, nil
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprintf("%f", balance)
+	os.WriteFile(accountBalanceFileName, []byte(balanceText), 0644)
+}
 
 func main() {
-
-	var accountBalance = 1000
+	var accountBalance,err = ReadFromFile()
+	if err != nil{
+		fmt.Println("Error")
+		fmt.Println(err)
+	}
 	fmt.Println("Welcome to Go Bank!")
 	for {
 
@@ -17,38 +43,39 @@ func main() {
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
 
-		switch choice{
-			case 1:
-				fmt.Println("Your balance is: ", accountBalance)
-			case 2:
-				var depositAmount int
-			  fmt.Print("Enter the amount to deposit: ")
-			  fmt.Scan(&depositAmount)
-		  	if depositAmount <= 0{
-				   fmt.Println("Invalid amount")
-				   continue
-			  }else{
-			  	accountBalance += depositAmount
-			  	fmt.Println("Your new balance is: ", accountBalance)
-			  }
-			case 3:
-			var withdrawAmount int
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is: ", accountBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Print("Enter the amount to deposit: ")
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount")
+				continue
+			} else {
+				accountBalance += depositAmount
+				fmt.Println("Your new balance is: ", accountBalance)
+				writeBalanceToFile(accountBalance)
+			}
+		case 3:
+			var withdrawAmount float64
 			fmt.Print("Enter the amount to withdraw: ")
 			fmt.Scan(&withdrawAmount)
-			if withdrawAmount > accountBalance{
+			if withdrawAmount > accountBalance {
 				fmt.Println("Insufficient funds")
-			}else{
+			} else {
 				accountBalance -= withdrawAmount
 				fmt.Println("Your new balance is: ", accountBalance)
+				writeBalanceToFile(accountBalance)
 			}
-			case 4:
+		case 4:
 			fmt.Println("Thank you for using Go Bank!")
-			default:
+		default:
 			fmt.Println("Invalid choice")
-			
+
 		}
-		
+
 	}
-	
-	
+
 }
